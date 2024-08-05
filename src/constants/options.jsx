85eef5331913510713488c28d0e2e49f474ef1f7
pothirendirahul <pothirendi.r@northeastern.edu>
2024-08-05 11:41:04 +0530

    Create-Trip(01)

---
+++ src/constants/options.jsx
@@ -0,0 +1,70 @@
+export const SelectTravelersList = [
+    {
+      id: 1,
+      title: 'Just Me',
+      desc: 'A sole traveler in exploration',
+      icon: '✈️',
+      people: '1'
+    },
+    {
+      id: 2,
+      title: 'A Couple',
+      desc: 'Two travelers in tandem',
+      icon: '👩‍❤️‍👨👩‍❤️‍👨👯',
+      people: '2 People'
+    },
+    {
+      id: 3,
+      title: 'Family',✈️
+      desc: 'A family adventure',
+      icon: '👩‍❤️‍👨',
+      people: '4 People'
+    },
+    {
+      id: 4,
+      title: 'Friends',
+      desc: 'A group of friends',
+      icon: '👫',
+      people: '3-5 People'
+    },
+    {
+      id: 5,
+      title: 'Team Outing',
+      desc: 'A corporate team outing',
+      icon: '👫',
+      people: '5-10 People'
+    }
+]
+
+export const SelectBudgetOptions = [
+    {
+      id: 1,
+      title: 'Budget',
+      desc: 'Affordable travel with basic amenities',
+      icon: '💰',
+      range: '$'
+    },
+    {
+      id: 2,
+      title: 'Standard',
+      desc: 'Comfortable travel with good amenities',
+      icon: '💵',
+      range: '$$'
+    },
+    {
+      id: 3,
+      title: 'Premium',
+      desc: 'High-quality travel with excellent amenities',
+      icon: '💎',
+      range: '$$$'
+    },
+    {
+      id: 4,
+      title: 'Luxury',
+      desc: 'Luxury travel with top-notch amenities and services',
+      icon: '🛎️',
+      range: '$$$$'
+    },
+];
+  
+export const AI_PROMPT='Generate Travel Plan for Location : {Location}'
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import { Input } from "../components/ui/input"; // Adjusted path to the Input component
 
-function  CreateTrip(){
+function CreateTrip() {
+  const [place, setPlace] = useState(null);
+
   return (
-    <div>
-      CreateTrip
+    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
+      <h2 className='font-bold text-3xl '>Tell us your travel preferences 🏕️🌴</h2>
+      <p className='mt-3 text-gray-500 text-xl'>
+        Give us a glimpse of your preferences, and our trip planner will craft an exclusive itinerary that reflects your unique style.
+      </p>
+      <div className='mt-20 flex flex-col gap-10'>
+        <div>
+          <h2 className='text-xl my-3 font-medium'>What is your destination of choice?</h2>
+          <GooglePlacesAutocomplete
+            apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+            selectProps={{
+              value: place,
+              onChange: (v) => { setPlace(v); console.log(v); }
+            }}
+          />
+        </div>
+        <div>
+          <h2 className='text-xl my-3 font-medium'>How many days are you planning your trip?</h2>
+          <Input placeholder="Ex. 3" type="number" />
+        </div>
+
+        <div>
+        <h2 className='text-xl my-3 font-medium'>What is Your Budget?</h2>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CreateTrip
- 
+export default CreateTrip;
