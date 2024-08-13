982d31ffc03635ad846401dbbc42e13d8a5a325d
pothirendirahul <pothirendi.r@northeastern.edu>
2024-08-13 08:14:50 +0530

    Footer

--- src/view-trip/components/Footer.jsx
+++ src/view-trip/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='mt-7'>
-      <h2 className='text-center text-gray-400'>Created By Rahul Pothirendi</h2>
+      <h2 className='text-center text-gray-500'>Developed by Ai-Trip-Planner Team - All Rights Reserved</h2>
     </div>
   )
 }
